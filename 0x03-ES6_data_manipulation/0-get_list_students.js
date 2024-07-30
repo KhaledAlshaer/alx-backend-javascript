22967fb3ec1c33ca4c76365b839885b316e73677
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-07-30 20:39:00 +0000

    0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default getListStudents() {
 	return [
 		{
-                id: 1,
-                firstName: 'Guillaume',
-                location: 'San Francisco',
-        },
-        {
-                id: 2,
-                firstName: "James",
-                location: "Columbia",
-        },
-        {
-                id: 3,
-                firstName: "Serena",
-                location: San Francisco,
-        },
+			id: 1,
+			firstName: 'Guillaume',
+			location: 'San Francisco',
+		},
+		{
+			id: 2,
+			firstName: "James",
+			location: "Columbia",
+		},
+		{
+			id: 3,
+			firstName: "Serena",
+			location: San Francisco,
+		},
 	];
 }
