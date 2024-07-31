456ae48789ea6b768ba880f6b8d7dfcca404b768
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-07-31 17:01:26 +0000

    1

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,39 @@
+interface Teacher {
+	firstName: string,
+	lastName: string,
+	fullTimeEmployee: boolean,
+	yearsOfExperience?: number,
+	location: string,
+	contract?: boolean,
+	[key: string]: any
+}
+
+interface Directors extends Teacher {
+	numberOfReports: number
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+	return `${firstName.charAt(0)}. ${lastName}`
+}
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+class StudentClass {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName,
+		this.lastName = lastName
+	}
+
+	workOnHomework() {
+		return "Currently working"
+	}
+
+	displayName() {
+		return this.firstName;
+	}
+}
