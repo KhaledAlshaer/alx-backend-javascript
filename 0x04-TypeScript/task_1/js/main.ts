32219696d4a932e0efa085437f850d48b935a5f4
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-07-31 17:07:51 +0000

    1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,7 +30,7 @@ interface StudentClassInterface {
   displayName(): string;
 }
 
-class StudentClass {
+class StudentClass implements StudentClassInterface {
 	firstName: string;
 	lastName: string;
 
